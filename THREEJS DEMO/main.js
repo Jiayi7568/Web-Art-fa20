a9ae10c1bd9912e55f354e819d7c02ae8fbd0cf8
Jiayi7568 <70674010+Jiayi7568@users.noreply.github.com>
2020-11-17 20:37:35 +0800

    add change

---
+++ THREEJS DEMO/main.js
@@ -0,0 +1,10 @@
+import {
+  BoxBufferGeometry,
+  Color,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+  DirectionalLight,
+} from './libs/threejs/build/three.module.js'
